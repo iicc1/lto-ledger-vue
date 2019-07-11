75aa26bc2a31fe7f588b6c491b7149762769b9bc
iicc1 <ignacio.iglesias@hotmail.es>
2019-07-11 13:32:08 +0200

    First design idea

--- src/components/LedgerWallet.vue
+++ src/components/LedgerWallet.vue
@@ -1,28 +1,70 @@
 <template>
     <div class="hello">
-        <div>
-            <div>
-                <b>User ID:</b>
+        <section class="hero is-primary is-medium">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        LTO Network
+                    </h1>
+                    <h2 class="subtitle">
+                        Ledger Wallet Interface
+                    </h2>
+                </div>
             </div>
-            <div>
-                <label>
-                    <input type="text" v-model="userId" @input="handleReset" />
-                </label>
+        </section>
+
+        <div class="container">
+            <div class="notification">
+                <b-field label="Choose a LTO Network wallet from your Ledger device">
+                    <b-select size="is-medium" placeholder="Choose one" rounded required>
+                        <option value="0">3Jn8EFisiWfiW1E9a4a1SLJepPN7bqGTvQs</option>
+                        <option value="1">3Jn8EFisiWfiW1E9a4a1SLJepPN7bqGTvQs</option>
+                        <option value="2">3Jn8EFisiWfiW1E9a4a1SLJepPN7bqGTvQs</option>
+                        <option value="3">3Jn8EFisiWfiW1E9a4a1SLJepPN7bqGTvQs</option>
+                        <option value="4">3Jn8EFisiWfiW1E9a4a1SLJepPN7bqGTvQs</option>
+                        <option value="5">3Jn8EFisiWfiW1E9a4a1SLJepPN7bqGTvQs</option>
+                    </b-select>
+                </b-field>
             </div>
         </div>
-        <div v-if="userInfo">
-            <div><b>User Info:</b></div>
-            <div>
-                <pre>{{userInfo}}</pre>
+
+        <div class="container">
+            <div class="notification">
+                <b-field label="Select the transaction type">
+                    <b-select size="is-medium" placeholder="Choose one" rounded required>
+                        <option value="4">Transfer</option>
+                        <option value="8">Lease Start</option>
+                        <option value="9">Lease Cancel</option>
+                        <option value="11">Mass transfer</option>
+                        <option value="15">Anchor</option>
+                    </b-select>
+                </b-field>
             </div>
         </div>
-        <div>
-            <button @click="handleConnect">Connect</button>
+
+        <div class="container">
+            <div class="notification">
+                <b-field label="Enter the recipient address">
+                    <b-input placeholder="Address" rounded></b-input>
+                </b-field>
+            </div>
         </div>
+
+        <div class="container">
+            <div class="notification">
+                <b-button type="is-primary">Sign transaction with Ledger</b-button>
+            </div>
+        </div>
+
+
     </div>
 </template>
 
 <script src="./logic.js">
+
+    export default {
+        components: {}
+    }
 
 </script>
 
--- src/components/logic.js
+++ src/components/logic.js
@@ -1,6 +1,6 @@
 import AsyncLock from 'async-lock'
 import TransportU2F from '@ledgerhq/hw-transport-u2f'
-import { WavesLedger } from '@waves/ledger'
+import {WavesLedger} from '@waves/ledger'
 
 const LOCK = 'LedgerDevice';
 
@@ -9,17 +9,16 @@ class LedgerDevice {
     this.lock = new AsyncLock()
   }
 
-  async getAddressInfo (userId, networkCode = 76 /*lto network mainnet code*/) {
+  async getAddressInfo (userId, networkCode = 76) {
     return this._safeExec(
       async () => {
         const app = new WavesLedger({
           transport: TransportU2F,
           exchangeTimeout: 10000,
           networkCode,
-          debug: false
+          debug: true
         });
-        const data = await app.getUserDataById(userId);
-        return data
+        return await app.getUserDataById(userId)
       }
     )
   }
@@ -32,12 +31,12 @@ class LedgerDevice {
   }
 }
 
-const device = new LedgerDevice()
+const device = new LedgerDevice();
 
 export default {
   data () {
     return {
-      userId: 0,
+      userId: 1,
       userInfo: null
     }
   },
@@ -46,8 +45,7 @@ export default {
       this.userInfo = null
     },
     async handleConnect () {
-      // this.userInfo =
-      this.userInfo = await device.getAddressInfo(this.userId, 76 /*lto network mainnet code*/)
+      this.userInfo = await device.getAddressInfo(this.userId, 76)
     }
   }
 }
