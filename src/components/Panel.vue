81842792da4f7aa2d002ee51c9cafe889f132f68
iicc1 <ignacio.iglesias@hotmail.es>
2019-07-17 17:17:55 +0200

    Added input validations and data

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,11 +1,12 @@
 <template>
     <section>
         <b-field grouped>
-            <b-field label="Select your wallet" expanded custom-class="is-size-5">
-                <b-input placeholder="Loading wallets from Ledger" loading icon-pack="fas" size="is-medium"></b-input>
+            <b-field label="Select your Ledger address" expanded custom-class="is-size-5" v-bind:type="addressOk">
+                <b-input v-if=address icon-pack="fas" size="is-medium" v-bind:value="address" disabled></b-input>
+                <b-input v-else icon-pack="fas" size="is-medium" placeholder="Loading address from Ledger device" loading></b-input>
             </b-field>
-            <b-field label="Wallet ID" custom-class="is-size-5" v-model="userId" v-on:change="handleConnect">
-                <b-select placeholder="0" size="is-medium" >
+            <b-field label="Address ID" custom-class="is-size-5">
+                <b-select placeholder="0" size="is-medium" @input="handleConnect">
                     <option v-for="n in 10" >{{ n - 1 }}</option>
                 </b-select>
             </b-field>
@@ -13,7 +14,7 @@
 
         <b-field grouped>
             <b-field label="Transaction type" custom-class="is-size-5">
-                <b-select placeholder="Select a type" size="is-medium">
+                <b-select placeholder="Select a type" size="is-medium" @input="transactionTypeSelection">
                     <option value="4">Transfer</option>
                     <option value="8">Start Lease</option>
                     <option value="9">Cancel Lease</option>
@@ -21,21 +22,21 @@
                 </b-select>
             </b-field>
             <b-field label="Amount" expanded custom-class="is-size-5">
-                <b-input type="number" step=".00000001" placeholder="0,00" size="is-medium"></b-input>
+                <b-input type="number" step=".00000001" min="0" placeholder="0,00 LTO" size="is-medium" @input="amountSelection"></b-input>
             </b-field>
         </b-field>
 
-        <b-field label="To address" custom-class="is-size-5">
-            <b-input name="LTO Network address" expanded size="is-medium"></b-input>
+        <b-field label="To address" custom-class="is-size-5" v-bind:type="recipientOk">
+            <b-input name="LTO Network address" expanded size="is-medium" @input="recipientSelection"></b-input>
         </b-field>
 
         <b-field label="Transaction Fee" custom-class="is-size-5">
-            <b-input type="number" step=".00000001" placeholder="1,00" size="is-medium"></b-input>
+            <b-input type="number" step=".00000001" min="0.5" placeholder="1,00 LTO" size="is-medium" @input="feeSelection"></b-input>
         </b-field>
 
         <div class="has-text-centered">
             <br>
-            <b-button type="is-primary" size="is-medium">Sign transaction</b-button>
+            <b-button type="is-primary" size="is-medium" @click="signTransaction">Sign transaction</b-button>
             <br><br>
         </div>
 
--- src/components/logic.js
+++ src/components/logic.js
@@ -1,6 +1,6 @@
 import AsyncLock from 'async-lock'
 import TransportU2F from '@ledgerhq/hw-transport-u2f'
-import {WavesLedger} from '@waves/ledger'
+import {WavesLedger} from 'iicc1/lto-ledger-js-unofficial'
 
 const LOCK = 'LedgerDevice';
 
@@ -39,17 +39,53 @@ export default {
         return {
             userId: 1,
             userInfo: null,
-            selectedOptions: []
+            loading: true,
+            placeholder: "Loading addresses from Ledger",
+            address: null,
+            publicKey: null,
+            addressOk: null,
+            recipientOk: null,
+            transactionData: {
+                type: null,
+                amount: null,
+                recipient: null,
+                fee: null
+            }
         }
     },
     methods: {
         async handleReset() {
             this.userInfo = null
         },
-        async handleConnect() {
-            // eslint-disable-next-line
-            console.log("test");
-            this.userInfo = await device.getAddressInfo(this.userId, 76)
+        async handleConnect(id) {
+            console.log(id);
+            this.userId = id;
+            this.userInfo = await device.getAddressInfo(this.userId, 76);
+            if (this.userInfo.address) {
+                this.address = this.userInfo.address;
+                this.publicKey = this.userInfo.publicKey;
+                this.addressOk = "is-success";
+            }
+        },
+        async transactionTypeSelection(type) {
+            this.transactionData.type = type;
+        },
+        async amountSelection(amount) {
+            this.transactionData.amount = amount * 10000000;
+        },
+        async recipientSelection(recipient) {
+            if (/^(3[jJ]\w{33})$/.test(recipient)) {
+                this.transactionData.recipient = recipient;
+                this.recipientOk = "is-success";
+            } else {
+                this.recipientOk = "is-danger";
+            }
+        },
+        async feeSelection(fee) {
+            this.transactionData.fee = fee * 10000000;
+        },
+        async signTransaction() {
+            console.log(this.transactionData)
         }
     }
 }
