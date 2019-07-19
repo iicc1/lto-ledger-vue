88741dc6ec5707686d5f6ab82a29641cebc4d3c6
triku <triku25@gmail.com>
2019-07-19 18:09:50 +0200

    footer typo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,11 +2,7 @@
     <footer class="footer">
         <div class="content has-text-centered">
          <p>
-            Made by <a href="https://t.me/iicc1">@iicc1</a> using <a href="https://vuejs.org">Vue.js</a> and <a href="https://buefy.org">Buefy</a>
-         </p>
-         <p>
-             Hosted by <a href="https://www.netlify.com/">Netlify</a>
-         </p>
+            Made by <a href="https://t.me/iicc1">@iicc1</a> with help of <a href="https://t.me/unai93">@unai93</a> using <a href="https://vuejs.org">Vue.js</a> and <a href="https://buefy.org">Buefy</a>
             <p>
                 Do you need help? Join <a href="https://t.me/ltonetwork/">LTO Network Tech chat</a>
             </p>
